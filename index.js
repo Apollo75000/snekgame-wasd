bfd7d719b492c5218fe66818476e76f5ae0d5546
Apollo75000 <78597084+Apollo75000@users.noreply.github.com>
2021-03-10 14:37:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ function clearScreen(){
 
 function drawSnake(){
    
-    ctx.fillStyle = 'LightPink';
+    ctx.fillStyle = 'yellow';
     for(let i =0; i < snakeParts.length; i++){
         let part =  snakeParts[i];
         ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
